a8a7bcab96a5120021d17540655bb01cd6dcb360
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-06-29 13:59:02 +0300

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,31 +1,31 @@
-import styles from './Statistics.module.css';
-import PropTypes from 'prop-types';
+// import styles from './Statistics.module.css';
+// import PropTypes from 'prop-types';
 
-export const Statistics = ({ title, stats }) => {
-  
-  return (
-    <section className={styles.statistics}>
-      {title && <h2 className={styles.title}>{title}</h2>}
+// export const Statistics = ({ title, stats }) => {
 
-      <ul className={styles.statsList}>
-        {stats.map(item => (
-          <li className={styles.item} key={item.id}>
-            <span className={styles.label}>{item.label}</span>
-            <span className={styles.percentage}>{item.percentage}</span>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-};
+//   return (
+//     <section className={styles.statistics}>
+//       {title && <h2 className={styles.title}>{title}</h2>}
 
-Statistics.propTypes = {
-  title: PropTypes.string,
-  item: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    })
-  ),
-};
+//       <ul className={styles.statsList}>
+//         {stats.map(item => (
+//           <li className={styles.item} key={item.id}>
+//             <span className={styles.label}>{item.label}</span>
+//             <span className={styles.percentage}>{item.percentage}</span>
+//           </li>
+//         ))}
+//       </ul>
+//     </section>
+//   );
+// };
+
+// Statistics.propTypes = {
+//   title: PropTypes.string,
+//   item: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       label: PropTypes.string.isRequired,
+//       percentage: PropTypes.number.isRequired,
+//     })
+//   ),
+// };
